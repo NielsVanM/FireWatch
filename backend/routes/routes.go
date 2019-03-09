59dfd750a737ca3fb4f05c73a2332ea3ec0df1be
NielsVanM <niels.van.marion@quicknet.nl>
2019-03-09 16:53:20 +0100

    Renamed Watcher -> backend and removed frontend files

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -3,27 +3,19 @@ package routes
 import (
 	"github.com/nielsvanm/firewatch/api"
 	"github.com/nielsvanm/firewatch/core/server"
-	"github.com/nielsvanm/firewatch/views"
 )
 
 // UnprotectedRoutes is a collection of routes not protected by the authorization
 // middleware
 var UnprotectedRoutes = server.NewRoute("", "GET", nil,
 	server.NewRoute("/auth/", "GET", nil,
-		server.NewRoute("login/", "GET,POST", views.LoginView, nil),
-		server.NewRoute("logout/", "POST", views.LogoutView, nil),
+		server.NewRoute("login/", "GET,POST", nil, nil),
+		server.NewRoute("logout/", "POST", nil, nil),
 	),
 )
 
 // ProtectedRoutes is a collection of routes protected by the authorization middleware
 var ProtectedRoutes = server.NewRoute("", "GET", nil,
-	server.NewRoute("/", "GET", views.DashboardView, nil),
-	server.NewRoute("/account/", "GET", views.AccountView,
-		server.NewRoute("all-device-logout/", "POST", views.LogOutAllDevicesView, nil),
-		server.NewRoute("change-password/", "GET,POST", views.ChangePasswordView, nil),
-		server.NewRoute("delete/", "GET,POST", views.DeleteAccountView, nil),
-	),
-	server.NewRoute("/device/", "GET", views.DeviceOverview, nil),
 	server.NewRoute("/api/", "GET", nil,
 		server.NewRoute("device/", "GET", api.GetAllDevices,
 			server.NewRoute("{id}/", "GET", api.GetDevice, nil),
